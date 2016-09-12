02e80bba9eb64d122dde0c4f5eda9c77063f8a9b
Jens Ildal Nielsen <JensIldal@Jenss-MacBook-Pro.local>
2016-09-12 11:23:21 +0200

    test2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Jens er gud");
+
+        nuller nulz = new nuller();
+        nulz.nulz();
     }
 }
---
+++ src/nuller.java
@@ -0,0 +1,12 @@
+/**
+ * Created by JensIldal on 12/09/2016.
+ */
+public class nuller {
+
+    public void nulz() {
+
+
+        System.out.println("nuller spiller smart");
+
+    }
+}
